fca3bcd3e740bf88b14d9071e827527b041169cc
swg <stewatwo@cpan.org>
2025-12-31 13:35:59 +0100

    More refactoring

--- common/src/AoC/Common.hs
+++ common/src/AoC/Common.hs
@@ -3,7 +3,7 @@ Module      : AoC.Common
 Description : A set of common functions to use in the daily tasks.
 Stability   : experimental
 
-* Output
+= Output
 
 @AoC.Common.Output@ includes helper functions to print pretty answers
 to the terminal. This module is mainly used for the runner, but also
@@ -13,7 +13,7 @@ exports the type @Day@.
   today = Day { year = 2025, day = 31 }
 @
 
-* Runner
+= Runner
 
 @AoC.Common.Runner@ has a function that runs the tasks given the day's
 input and prints the results to the console.
